f73d10611dcecd542accc0dbb47002cc94be61c8
nignanthomas <nignanthomas@gmail.com>
2018-10-02 23:46:21 +0300

    create 20 doctors in doctors-database.js

--- js/doctors-database.js
+++ js/doctors-database.js
@@ -9,3 +9,24 @@ var Doctor = function(title, firstName, lastName, location, specialty, gender, r
    this.photo = photo;
    this.biography = biography;
 }
+
+var doctor1= new Doctor("Dr","Okerio","Clinton","CBD","Gynaecologist","M",3,"photo","Senior Lecturer at The University Of Nairobi school of medicine, chief Gynaecologist at Nairobi Hospital")
+var doctor2= new Doctor("Dr","Kanoga","Ndegwa","CBD","Paeditrician","M",5,"photo","Over 15 years experience dealing with children in Hospitals around and out of Kenya")
+var doctor3= new Doctor("Pr","Nignan","Thomas","CBD","Opthalmologist","M",5,"photo","Founder of eye foundation Kenya and lead researcher angaza macho")
+var doctor4= new Doctor("Dr","Omar","Abdiwahab","CBD","Dentist","M",4,"photo","worked for W.H.O in Sudan, Ethiopia, Somalia, Djibouti and Kenya")
+var doctor5= new Doctor("Dr","Nyakio","Mburu","Westlands","Cardiologist","F",5,"photo","Specialist in cardiac attacks and history of the heart")
+var doctor6= new Doctor("Dr","Waiyaki","Maxwell","Westlands","General Practitioner","M",5,"photo","Wide experience in general medicine and an associate professor at Moi university")
+var doctor7= new Doctor("Pr","Nyendwe","Cliff","Westlands","Paeditrician","M",3,"photo","Founder of Kick Polio Campaign in Kenya")
+var doctor8= new Doctor("Dr","Mulyungi","Diana","Westlands","Dentist","F",5,"photo","Cosmetic and General dentist at Kenyatta Hospital")
+var doctor9= new Doctor("Dr","Ram","Naika","Embakasi","Gynaecologist","M",4,"photo","Harvard University school of Medicine Alumni")
+var doctor10= new Doctor("Dr","Gesaka","Rael","Embakasi","General Practitioner","F",5,"photo","Lead consultant Aghakan Hospital")
+var doctor11= new Doctor("Pr","Mauta","Stephen","Embakasi","Paeditrician","M",5,"photo","Co-founder Afya bora kwa Watoto")
+var doctor12= new Doctor("Dr","Kangangi","Marvin","Embakasi","Cardiologist","M",4,"photo","Surgeon at Nairobi Hospital and Karen Hospital")
+var doctor13= new Doctor("Dr","Sheila","Aluoch","Kibra","General Practitioner","F",3,"photo","John Hopkins graduate 2007 and member of W.H.O")
+var doctor14= new Doctor("Pr","Jamleck","Ndung'u","Kibra","Gynaecologist","M",4,"photo","Lead team in the First test-tube babies production")
+var doctor15= new Doctor("Dr","Hamida","Mstafa","Kibra","Dentist","F",5,"photo","Cosmetic and General dentist at Aghakan Hospital")
+var doctor16= new Doctor("Dr","Rose","Oketch","Kibra","Paeditrician","F",5,"photo","Co-founder Afya bora kwa Watoto")
+var doctor17= new Doctor("Pr","Raphael","Katana","Dagoretti","Opthalmologist","M",4,"photo","Specialist in contact lenses and general eye problems")
+var doctor18= new Doctor("Dr","Gloria","Givondo","Dagoretti","Dentist","F",5,"photo","Founder of Smile Kenya")
+var doctor19= new Doctor("Dr","Chris","Karinge","Dagoretti","General Practitioner","M",4,"photo","Wide experience in general medicine and an associate professor at Moi university")
+var doctor20= new Doctor("Dr","Oliver","Akwenda","Dagoretti","Cardiologist","M",5,"photo","Over 15 years experience dealing with cardiac surgeries in Hospitals around and out of Kenya")
