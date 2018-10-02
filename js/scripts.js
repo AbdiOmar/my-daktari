acf9fea6aa7e5a6b89f4c7acee613b1c5f2df2e8
nignanthomas <nignanthomas@gmail.com>
2018-10-02 23:29:48 +0300

    add few changes to search-doctor.html; add script to display doctors according to search criterias

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,98 @@
-$(document).ready(function(){
-  $(".display-profiles").hide()
-  $(".book").hide();
+$(".display-profiles").hide()
+$(".book").hide();
 
+$(document).ready(function(){
   $(".search-doctor").submit(function(event){    // submit search
     event.preventDefault();
     var selectedLocation = $(".select-location").val();
     var selectedSpecialty = $(".select-specialty").val();
+
+    $(".display-profiles").show();
+
+    if (selectedSpecialty == null && selectedLocation == null) {
+      doctors.forEach(function(doctor){
+        $(".display-profiles").append('<div class="doctor-profile col-md-3">' +
+                                        '<div>' +
+                                          '<img src="'+doctor.photo+'" alt="Profile Photo">' +
+                                          '<p>'+doctor.title + ' ' + doctor.firstName + ' ' + doctor.lastName+'</p>' +
+                                        '</div>' +
+                                        '<div>' +
+                                          '<p>'+doctor.specialty+'</p>' +
+                                          '<p>'+doctor.location+'</p>' +
+                                          '<p>'+doctor.gender+'</p>' +
+                                          '<p> <img src="images/'+doctor.ratings+'stars.png" alt="'+doctor.ratings+' stars"> </p>' +
+                                        '</div>' +
+                                        '<div>' +
+                                          '<p>'+doctor.biography+'</p>' +
+                                        '</div>' +
+                                        '<button class="book-profile" type="button" name="button"> BOOK </button>' +
+                                      '</div>')
+      }); //end loop arrayDoctors
+    } else if (selectedLocation == null) {
+      doctors.forEach(function(doctor){
+        if (doctor.specialty == selectedSpecialty) {
+          $(".display-profiles").append('<div class="doctor-profile col-md-3">' +
+                                          '<div>' +
+                                            '<img src="'+doctor.photo+'" alt="Profile Photo">' +
+                                            '<p>'+doctor.title + ' ' + doctor.firstName + ' ' + doctor.lastName+'</p>' +
+                                          '</div>' +
+                                          '<div>' +
+                                            '<p>'+doctor.specialty+'</p>' +
+                                            '<p>'+doctor.location+'</p>' +
+                                            '<p>'+doctor.gender+'</p>' +
+                                            '<p> <img src="images/'+doctor.ratings+'stars.png" alt="'+doctor.ratings+' stars"> </p>' +
+                                          '</div>' +
+                                          '<div>' +
+                                            '<p>'+doctor.biography+'</p>' +
+                                          '</div>' +
+                                          '<button class="book-profile" type="button" name="button"> BOOK </button>' +
+                                        '</div>')
+        }
+      }); //end loop arrayDoctors
+    } else if (selectedSpecialty == null) {
+      doctors.forEach(function(doctor){
+        if (doctor.location == selectedLocation) {
+          $(".display-profiles").append('<div class="doctor-profile col-md-3">' +
+                                          '<div>' +
+                                            '<img src="'+doctor.photo+'" alt="Profile Photo">' +
+                                            '<p>'+doctor.title + ' ' + doctor.firstName + ' ' + doctor.lastName+'</p>' +
+                                          '</div>' +
+                                          '<div>' +
+                                            '<p>'+doctor.specialty+'</p>' +
+                                            '<p>'+doctor.location+'</p>' +
+                                            '<p>'+doctor.gender+'</p>' +
+                                            '<p> <img src="images/'+doctor.ratings+'stars.png" alt="'+doctor.ratings+' stars"> </p>' +
+                                          '</div>' +
+                                          '<div>' +
+                                            '<p>'+doctor.biography+'</p>' +
+                                          '</div>' +
+                                          '<button class="book-profile" type="button" name="button"> BOOK </button>' +
+                                        '</div>')
+        }
+      }); //end loop arrayDoctors
+    } else if (selectedSpecialty != null && selectedLocation != null) {
+      doctors.forEach(function(doctor){
+        if (doctor.specialty == selectedSpecialty && doctor.location == selectedLocation) {
+          $(".display-profiles").append('<div class="doctor-profile col-md-3">' +
+                                          '<div>' +
+                                            '<img src="'+doctor.photo+'" alt="Profile Photo">' +
+                                            '<p>'+doctor.title + ' ' + doctor.firstName + ' ' + doctor.lastName+'</p>' +
+                                          '</div>' +
+                                          '<div>' +
+                                            '<p>'+doctor.specialty+'</p>' +
+                                            '<p>'+doctor.location+'</p>' +
+                                            '<p>'+doctor.gender+'</p>' +
+                                            '<p> <img src="images/'+doctor.ratings+'stars.png" alt="'+doctor.ratings+' stars"> </p>' +
+                                          '</div>' +
+                                          '<div>' +
+                                            '<p>'+doctor.biography+'</p>' +
+                                          '</div>' +
+                                          '<button class="book-profile" type="button" name="button"> BOOK </button>' +
+                                        '</div>')
+        }
+      }); //end loop arrayDoctors
+    }; //end of the control flow to display doctors profiles
     
 
-
-  }) //end submit-search
+  }); //end submit-search
 }); //end ready
