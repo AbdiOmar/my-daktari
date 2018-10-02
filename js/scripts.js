cc9b4a0b74709643da8a55e696d23107864fcdad
nignanthomas <nignanthomas@gmail.com>
2018-10-02 10:47:39 +0300

    add script to return the values of select inputs

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
   $(".display-profiles").hide()
   $(".book").hide();
 
-  
-});
+  $(".search-doctor").submit(function(event){    // submit search
+    event.preventDefault();
+    var selectedLocation = $(".select-location").val();
+    var selectedSpecialty = $(".select-specialty").val();
+    
+
+
+  }) //end submit-search
+}); //end ready
