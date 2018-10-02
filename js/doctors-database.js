a7dd1e5498f8ee7cff0353eff6966225953a8728
nignanthomas <nignanthomas@gmail.com>
2018-10-02 23:44:21 +0300

    create Doctor constructor in doctors-database.js

---
+++ js/doctors-database.js
@@ -0,0 +1,11 @@
+var Doctor = function(title, firstName, lastName, location, specialty, gender, ratings, photo, biography){
+   this.title = title;
+   this.firstName = firstName;
+   this.lastName = lastName;
+   this.location = location;
+   this.specialty = specialty;
+   this.gender = gender;
+   this.ratings = ratings;
+   this.photo = photo;
+   this.biography = biography;
+}
